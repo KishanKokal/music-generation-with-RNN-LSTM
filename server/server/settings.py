5a76e575178011b9fe2c066f419d7ffde895d125
Kishan Kokal <kokalkishan.official@gmail.com>
2024-04-04 15:07:39 +0530

    Update ALLOWED_HOSTS in settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-soow2%osg-@bx-i_zycuel&#441k%(+yh4o-j!s$hxtw=@(pge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
